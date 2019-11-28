6dff0c3ad19b168f7ac4c2e53f27b6fcc2a69b4d
Athena Chen <athenachenn@gmail.com>
2019-11-28 13:32:56 -0500

    Updates

---
+++ precache-manifest.afb863ba3b02d413a2c8ad158f9bd8e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cf3224f519cede0a2407e535b2f7b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6071a5092284fc2d4774",
+    "url": "/static/css/main.f88686e5.chunk.css"
+  },
+  {
+    "revision": "a003f9af0da78ed3e02b",
+    "url": "/static/js/2.bc1f6c89.chunk.js"
+  },
+  {
+    "revision": "6071a5092284fc2d4774",
+    "url": "/static/js/main.1583b071.chunk.js"
+  },
+  {
+    "revision": "4d5b4449eca69dca7740",
+    "url": "/static/js/runtime-main.77d3c816.js"
+  },
+  {
+    "revision": "6b0f478ab017b7d1bc309ec8ee4012d1",
+    "url": "/static/media/cloak-n-dogger-thumbnail.6b0f478a.jpg"
+  },
+  {
+    "revision": "6aefc0403fab434182aa13ffa94defad",
+    "url": "/static/media/pictionary-thumbnail.6aefc040.jpg"
+  },
+  {
+    "revision": "36e510394ec76e619a32ae6f05f399c3",
+    "url": "/static/media/pictionary.36e51039.gif"
+  },
+  {
+    "revision": "58d256c13a902bb00c2e7abccd249486",
+    "url": "/static/media/unnamed.58d256c1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.0369be7c8ae0517d02fd22022b659ef7.js"
+  "/precache-manifest.afb863ba3b02d413a2c8ad158f9bd8e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

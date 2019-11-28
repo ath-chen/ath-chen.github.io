0c876cefe141155179a4312312421d77d7d6fa0e
Athena Chen <athenachenn@gmail.com>
2019-11-28 13:52:49 -0500

    Updates

---
+++ precache-manifest.0369be7c8ae0517d02fd22022b659ef7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993ce80b125c60fb1198875555a3e98d",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "e7c0c1ba132b295424dd",
+    "url": "/website/static/css/main.f88686e5.chunk.css"
+  },
+  {
+    "revision": "a003f9af0da78ed3e02b",
+    "url": "/website/static/js/2.bc1f6c89.chunk.js"
+  },
+  {
+    "revision": "e7c0c1ba132b295424dd",
+    "url": "/website/static/js/main.c91a7cc2.chunk.js"
+  },
+  {
+    "revision": "596830d9d2746410f827",
+    "url": "/website/static/js/runtime-main.16cc0d70.js"
+  },
+  {
+    "revision": "6b0f478ab017b7d1bc309ec8ee4012d1",
+    "url": "/website/static/media/cloak-n-dogger-thumbnail.6b0f478a.jpg"
+  },
+  {
+    "revision": "6aefc0403fab434182aa13ffa94defad",
+    "url": "/website/static/media/pictionary-thumbnail.6aefc040.jpg"
+  },
+  {
+    "revision": "36e510394ec76e619a32ae6f05f399c3",
+    "url": "/website/static/media/pictionary.36e51039.gif"
+  },
+  {
+    "revision": "58d256c13a902bb00c2e7abccd249486",
+    "url": "/website/static/media/unnamed.58d256c1.jpg"
+  }
+]);

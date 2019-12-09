322a5b0b7759b8ec8791550ed2d8d5f4c981c661
Athena Chen <athenachenn@gmail.com>
2019-12-08 22:46:43 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.38ae3903852b0675a120a7c156e03176.js"
+  "/website/precache-manifest.298deea75f9608a3fe32249aa610316f.js"
 );
 
 self.addEventListener('message', (event) => {
